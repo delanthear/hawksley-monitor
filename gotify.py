f2c1f80f336a6261f6806b777ef62fd895cf6639
delanthear <kellydorset+github@googlemail.com>
2025-03-06 17:30:44 +0000

    Update gotify.py
    Added new notification for max battery based off of config setting.  Updated gotify module to handle multiple notifications

--- gotify.py
+++ gotify.py
@@ -4,10 +4,10 @@ import requests
 import json
 import time
 
-def sendGotifyMessage(token, address, title, message, priority=5):
+def sendGotifyMessage(token, address, notificationKey, title, message, priority=5):
 
     # check if we've sent one yet    
-    if should_send_notification():
+    if should_send_notification(notificationKey):
         
         gotifyURL = address + "/message?token=" + token
 
@@ -24,33 +24,33 @@ def sendGotifyMessage(token, address, title, message, priority=5):
         print(f"Status Code: {response.status_code}")
         print(f"Response Body: {response.text}")
         
-        save_last_notification_date()  # Save today's date after sending notification
+        save_last_notification_date(notificationKey)  # Save today's date after sending notification
 
         return(response)
 
-# Function to read last notification date from the file
-def get_last_notification_date():
+# Function to read the last notification dates from the file
+def get_last_notification_dates():
     try:
         with open('last_notification.json', 'r') as file:
-            data = json.load(file)
-            return data.get('date')
+            return json.load(file)
     except FileNotFoundError:
-        # If the file does not exist, return None (first time running)
-        return None
+        # If the file does not exist, return an empty dictionary
+        return {}
 
-# Function to save the current date as the last notification date
-def save_last_notification_date():
+# Function to save the current dates for notifications
+def save_last_notification_date(notification_name):
+    data = get_last_notification_dates()
+    data[notification_name] = datetime.today().strftime('%Y-%m-%d')
     with open('last_notification.json', 'w') as file:
-        json.dump({'date': datetime.today().strftime('%Y-%m-%d')}, file)
+        json.dump(data, file)
 
-
-# Function to check if the notification has already been sent today
-def should_send_notification():
+# Function to check if a specific notification has already been sent today
+def should_send_notification(notification_name):
     current_date = datetime.today().strftime('%Y-%m-%d')
-    last_notification_date = get_last_notification_date()
+    last_notification_dates = get_last_notification_dates()
 
-    if last_notification_date != current_date:
-        # If last notification date is not today's date, allow sending the notification
+    if last_notification_dates.get(notification_name) != current_date:
+        # If the last notification date for this name is not today's date, allow sending
         return True
     else:
         # Otherwise, don't send it
